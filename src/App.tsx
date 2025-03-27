0214bce15bfe784d054cf11068cef827d06e07e1
Bianca Santos <luisabianca93@gmail.com>
2025-03-26 22:55:32 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='main'>
+      <p>Luiza by Tech. In Memoriam</p>
+      <img src="src/assets/mamis.jpg" alt="" style={{width:'500px',height:'auto'}}/>
+    </div>
+  )
+}
+
+export default App
